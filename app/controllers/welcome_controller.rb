ab9f70f83a8c61e70487a9ce52310d6083470dfa
rdelarosa3 <rdelarosa39@gmail.com>
2019-01-21 15:00:56 +0700

    set hashtag keyword

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def homepage
+  		@business = Business.first
   		@staff = User.artist.all
 	  	# @service_types = ServiceType.all.order('name ASC')
 	  	@request = Request.new
