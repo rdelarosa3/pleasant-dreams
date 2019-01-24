a910a323fb26ea10fcaeb5882fc8d518f8996ebd
rdelarosa3 <rdelarosa39@gmail.com>
2019-01-24 21:24:02 +0700

    fixed mailer

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -4,11 +4,11 @@ class RequestMailer < ApplicationMailer
        mail(to: @request.email, subject: "Pleasant Dreams Co. Inquiry") 
    end
 
-   def new_request(request, users)
-   		@request = request
-   		@users = users
-   		@users.each do |user|
-   			mail(to: user.email), subject: "Pleasant Dreams Co. NEW Inquiry"
-   		end
-   end
+  def new_request(request, users)
+  		@request = request
+  		@users = users
+  		@users.each do |user|
+  			mail(to: user.email), subject: "Pleasant Dreams Co. NEW Inquiry")
+  		end
+  end
 end
