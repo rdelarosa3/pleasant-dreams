91e20a71bbf9c0de4dd202c482a0f176fbfbe50c
rdelarosa3 <rdelarosa39@gmail.com>
2019-01-24 18:27:18 +0700

    fixed gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
-   :port                 => 587,
+   :port                 => 465,#587,
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
